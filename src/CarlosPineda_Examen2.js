aababde4eda234359f3d5060e434a230991646c4
canpine <candresp26@gmail.com>
2022-05-13 11:07:35 -0500

    Se modifica Examen Punto2

--- src/CarlosPineda_Examen2.js
+++ src/CarlosPineda_Examen2.js
@@ -3,3 +3,14 @@ while(result>360){
     result=result-20;
     console.log(result);
 }
+var mostrar = 680;
+for(let i=0;i<340; i++){
+
+    mostrar = mostrar -20;
+    if(mostrar<340){
+    break;
+    }
+    console.log(mostrar);
+
+}
+
