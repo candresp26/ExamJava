af92ad313734a8afa1ca37b4a85ba799f68a41ac
canpine <candresp26@gmail.com>
2022-05-13 12:23:46 -0500

    Se modifica Examen Punto4

--- src/CarlosPineda_Examen4.js
+++ src/CarlosPineda_Examen4.js
@@ -34,17 +34,40 @@ class Empleado extends Persona{
 
 class Nomina extends Empleado{
 
-    Pagos(){
+    constructor(Empleado = []){
+        super.getDetails;
+    }
+
+    pagos(){
         let aleatorio = Math.floor((Math.random() * (15-5)) +5);
         console.log(aleatorio)
-        if(Empleado.tipo="S"){
-            let Salsind  = (aleatorio * 350)*0.10;
+        for (let index = 0; index < Empleado.length; index++) {
+            if(Empleado[index].tipo="S"){
+                let Salsind  = (aleatorio * 350)*0.10;
+                console.log( Empleado[index].nombre+" "+Empleado[index].apellido+" de "+ Empleado[index].edad+" pertenece a sindicato con un sueldo de "+ Salsind+" por "+aleatorio+" dias trabajados" )
 
 
-        }esle
-        {
-            let SalConfi = (aleatorio*500)*0.13;
+            }esle
+            {
+                let SalConfi = (aleatorio*500)*0.13;
+                console.log( Empleado[index].nombre+" "+Empleado[index].apellido+" de "+ Empleado[index].edad+" pertenece a empleado de confianza con un sueldo de "+ Salsind+" por "+aleatorio+" dias trabajados" )
+            }
+
         }
+
+    }
+
+
+    calcularNomina(){
+
+        let empleado1 = new Nomina ("Carlos","Pineda",30,C);
+        let empleado2 = new Nomina ("Andres","Perez",31,C);
+        let empleado3 = new Nomina ("Roberto","Jimenez",32,S);
+
+        empleado1.pagos;
+        empleado2.pagos;
+        empleado3.pagos;
+
     }
 
 
