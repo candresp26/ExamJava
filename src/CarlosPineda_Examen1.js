15dc0ddeccbe105dfb96c9eb8b206b62bf482b1c
canpine <candresp26@gmail.com>
2022-05-13 10:36:59 -0500

    Se modifica Examen Punto1

--- src/CarlosPineda_Examen1.js
+++ src/CarlosPineda_Examen1.js
@@ -1,7 +1,4 @@
 
-let horaExtra=350;
-
-let horasNormal=40;
 let horasTrabajador=53;
 
 if(horasTrabajador>40){
@@ -9,6 +6,6 @@ if(horasTrabajador>40){
     let pagoNormales = 40*265;
     let pagoExtra = extras * 350;
     console.log("El total a pagar por horas laborales normal (40hrs) se de : "+pagoNormales);
-    console.log("El total a pagar por horas extra"+extras+"hrs es de: "+pagoExtra);
+    console.log("El total a pagar por horas extra "+extras+" hrs es de: "+pagoExtra);
 
 }
